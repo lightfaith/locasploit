2ebc6c924c018a56a7f2966700b3e3265acd9f16
Vítězslav Grygar <lightfaith@users.noreply.github.com>
2016-02-04 20:55:00 +0100

    user threads

--- source/libs/scheduler.py
+++ source/libs/scheduler.py
@@ -10,18 +10,25 @@ class Job:
 		self.job = job
 		self.timeout = timeout
 
+
+
+
 class Scheduler(threading.Thread):
 	# takes care of modules running in the background
 	def __init__(self):
 		threading.Thread.__init__(self)
 		self.lock = threading.Lock()
 		self.jobs = {}
+		self.user_threads = []
 		self.terminate = False
-	
+
+
 	def run(self):
 		while not self.terminate: # quit program not requested
-			todel = []
 			self.lock.acquire()
+			todel = []
+
+			# BACKGROUND JOBS
 			for x in self.jobs: # pinpoint dead background jobs
 				s = self.jobs[x]
 				if not s.job.isAlive():
@@ -32,24 +39,41 @@ class Scheduler(threading.Thread):
 				end = time.time()
 				log.info('Background job %d (%s) has terminated (%s).' % (x, s.name, log.show_time(end-s.start)))
 				del self.jobs[x]
+			
+			# USER THREADS
+			todel = []
+			for t in self.user_threads: # pinpoint dead user threads
+				if not t.isAlive():
+					todel.append(t)
+
+			for t in todel: # remove them
+				self.user_threads.remove(t)
 
 			self.lock.release()
 			time.sleep(0.25)
 
 
+
 	def stop(self):
 		# scheduler should terminate (program is going to exit)
 		self.lock.acquire()
-		# terminate all jobs which supports it
+		# terminate all jobs and user threads which supports it
 		for j in self.jobs:
 			if hasattr(self.jobs[j].job, 'stop'):
 				self.jobs[j].job.stop()
 				#log.info('Background job %d is going to terminate.' % j)
+
+		for u in self.user_threads:
+			if hasattr(u, 'stop'):
+				u.stop()
+
 		self.lock.release()
 		while len(self.jobs) > 0: # wait for everything to die
 			time.sleep(0.5)
 		self.terminate = True # sets DIE flag for itself
-	
+
+
+
 	def add(self, name, start, job, timeout=None):
 		# add a new job
 		self.lock.acquire()
@@ -57,6 +81,8 @@ class Scheduler(threading.Thread):
 		self.jobs[jobid] = Job(name, start, job, timeout)
 		log.info('Module %s will run in the background with id %d.' % (name, jobid))
 		self.lock.release()
+
+
 
 	def newid(self):
 		# only called from add() => lock in place
@@ -66,6 +92,14 @@ class Scheduler(threading.Thread):
 				break
 			result += 1
 		return result
+
+
+
+	def add_user_thread(self, thread):
+		with self.lock:
+			self.user_threads.append(thread)
+	
+	
 	
 	def show(self):
 		now = time.time()
@@ -87,6 +121,8 @@ class Scheduler(threading.Thread):
 			x = keys[i]
 			log.writeline('%*s  %-*s  %*s  %-*s' % (maxi, x, maxn, self.jobs[x].name, maxt, times[i], maxto, '' if self.jobs[x].timeout is None else self.jobs[x].timeout))
 		self.lock.release()
+
+
 
 	def kill(self, jid):
 		# kill specified job
