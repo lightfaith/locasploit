68c5b9e5be343e45d1cccff102decb539fbec53c
Vítězslav Grygar <lightfaith@users.noreply.github.com>
2016-02-04 20:58:54 +0100

    v1.1 - threads, word splitting

--- source/modules/crypto_language.py
+++ source/modules/crypto_language.py
@@ -10,12 +10,11 @@ class Module(GenericModule):
 		self.name = 'crypto.language'
 		self.short_description = 'Attempts to reveal string\'s language.'
 		self.references = [
-		#	'',
 		]
 		
 		self.date = '2016-02-03'
 		self.license = 'GNU GPLv2'
-		self.version = '1.0'
+		self.version = '1.1'
 		self.tags = [
 			'cipher',
 			'language',
@@ -24,6 +23,7 @@ class Module(GenericModule):
 This module compares strings located at 'DATA > <key>' to dictionaries.
 For huge sets of data, this could take a long time. If possible, you should specify the KB key as accurate as possible. 
 Language code can be also specified.
+For some reason, adding threads does not help. This should be fixed...
 """
 		self.kb_access = [
 			'DATA',
@@ -32,6 +32,8 @@ Language code can be also specified.
 		self.dependencies = {
 		}
 		self.changelog = """
+1.1: Threading support (not quite efficient)
+1.0: language recognition
 """
 
 		self.reset_parameters()
@@ -41,6 +43,7 @@ Language code can be also specified.
 			'SILENT': Parameter(value='no', mandatory=True, description='Suppress the output'),
 			'KEY': Parameter(value='', mandatory=True, description='KB Data Key to analyze'),
 			'CODE': Parameter(value='', mandatory=False, description='Language code (empty = all)'),
+			'THREADS': Parameter(value='1', mandatory=True, description='Number of threads to use concurrently'),
 		}
 
 	def check(self):
@@ -61,60 +64,106 @@ Language code can be also specified.
 		if not lib.kb.exists('DATA %s' % key):
 			log.err('Key \'%s\' does not exist.' % key)
 			return None
-		
-		messages = []
+		if not self.parameters['THREADS'].value.isdigit():
+			log.err('THREADS parameter must be a number.')
+			return None
+		threadnum = int(self.parameters['THREADS'].value)
+
+		data = {}
+
 		# deal with message/messages
-		datakey = lib.kb.find('DATA %s' % key)[0]
+		datakey = lib.kb.find('DATA %s' % key)
 		if type(datakey) is list or type(datakey) is tuple:
-			messages = datakey
+			for i in range(len(datakey)):
+				data[i]=datakey[i]
 		elif type(datakey) is dict:
-			messages += list(datakey.values())
+			for x in datakey:
+				data[x] = datakey[x]
 		elif type(datakey) is bytes:
-			messages.append(datakey.decode('utf-8'))
+			data[key] = datakey.decode('utf-8')
 		elif type(datakey) is str:
-			messages.append(datakey)
+			data[key] = datakey
 		else:
 			log.err('Unexpected message type %s, exiting...' % type(datakey))
 			return None
 
-		messages = natural_sort(messages)
+		keys = list(data)
 		# for each string to analyze
-		for message in messages:
-			# prepare dictionary {language:rate}
-			score = {}
-			if len(code) > 0: # only one language
-				score[code] = 0
-			else: # all languages
-				for lang in lib.dicts:
-					score[lang] = 0
 
-			# for each language
-			for lang in list(score):
-				# for each word
-				words = re.findall(r"[A-Za-z'0-9]+", message)
-				for word in words:
-					if word.lower() in lib.dicts[lang]:
-						score[lang] += 1
-				score[lang] /= float(len(words))
+		# run desired number of threads
+		threads = []
+		for i in range(0, threadnum):
+			t = Thread({k:data[k] for k in keys[i::threadnum]})
+			t.start()
+			lib.scheduler.add_user_thread(t)
+			threads.append(t)
+		
+		counter = 0  # counts number of evaluations processed
+		while counter < len(keys):
+			# check if there is something in the pool
+			if len(Thread.dic) > 0:
+				key = list(Thread.dic)[0] # get the first key (basically random)
 
-			# print results
-			if not silent:
-				toshow = message.replace('\n', '\\n')
-				if max([score[x] for x in score]+[]) >= 0.5:
-					log.ok('Results for \'%s%s\'' % (toshow[:40], '...' if len(toshow) > 43 else ''))
-				else:
-					log.writeline('Analysis for \'%s%s\'' % (toshow[:40], '...' if len(toshow) > 43 else ''))
-				
-				sortscore = sorted(sorted(list(score), key=lambda x: score[x], reverse=True))
-				for lang in score:
-					line = '%*s: %7.3f%%' % (6, lang, score[lang] * 100.0)
-					if score[lang] >= 0.5:
-						log.ok(line)
-					else:
-						log.writeline(line)
-			log.writeline()
+				#print header
+				if not silent:
+					log.writeline()
+					toshow = data[key].replace('\n', '\\n')[:40]
+					log.writeline('Results for \'%s\' (%s%s):' % (key, toshow, '...' if len(data[key]) > 43 else ''))
+
+				# for each language
+				for lang in Thread.dic[key]:
+					# print results
+					if not silent:
+						value = Thread.dic[key][lang]
+						line = '%*s: %7.3f%%' % (6, lang, value * 100.0)
+						if value >= 0.5:
+							log.ok(line)
+						else:
+							log.writeline(line)
+
+				# update pool and counter
+				del Thread.dic[key]
+				counter += 1
+			else: # pool empty, wait for a while
+				time.sleep(1)
+
+
+		for t in threads:
+			t.join()
 		# # # # # # # #
 		return None
 	
+# Thread checks single message to multiple languages relationships
+class Thread(threading.Thread):
+	lock = threading.Lock()
+	dic = {}
+	
+	def __init__(self, data):
+		threading.Thread.__init__(self)
+		self.data = data
+		self.terminate = False
+		self.lock = threading.Lock()
+
+
+	def run(self):
+		for key in self.data:	
+			score = {}
+			for lang in lib.dicts:
+				if self.terminate:
+					break
+				score[lang] = 0.0
+				#words = re.findall("[A-Za-z'0-9]+", self.data[key])
+				#words = re.split(r"[\s\d!\?@#\$%\^&\*\(\)_\+{}[]:|'\"\\-.,]", self.data[key])
+				words = list(filter(None, re.split(r"[\s.!,?\-0-9@]", self.data[key])))
+				for word in words:
+					if word.lower() in lib.dicts[lang]:
+						score[lang] += 1
+
+				score[lang] /= float(len(words))
+			Thread.dic[key] = score
+
+	
+	def stop(self):
+		self.terminate = True
 
 lib.module_objects.append(Module())
