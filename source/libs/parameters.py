07d5dc87d219500cfc913532b13eff5398fd9a89
root <lightfaith@users.noreply.github.com>
2017-02-12 05:23:46 -0600

    cleanup

--- source/libs/parameters.py
+++ source/libs/parameters.py
@@ -6,5 +6,3 @@ class Parameter:
 		self.value=value 				# value
 		self.mandatory=mandatory		# if it must be defined
 		self.description=description	# description
-		#self.kb=kb						# if Knowledge Base item - will be checked for existence
-		#self.dependency=dependency		# if another module - will be checked for existence
