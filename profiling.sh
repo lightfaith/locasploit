79421c8221c33f95ba4d3c3f62f8e63f0bbd2079
root <lightfaith@users.noreply.github.com>
2017-02-12 04:55:15 -0600

    yappi profiling

--- profiling.sh
+++ profiling.sh
@@ -2,4 +2,6 @@
 # runs profile for locasploit -c input
 
 if [ $1 ]; then file=$1; else file='input'; fi
-python3 -m cProfile -s cumtime ./locasploit.py -c $file
+#python3 -m cProfile -s cumtime ./locasploit.py -c $file
+# pip install yappi; 
+yappi -b -o /tmp/profiled.callgrind -f callgrind ./locasploit.py -c $file
