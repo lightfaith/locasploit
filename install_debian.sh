479aecbb008063d64d37e91d4f6f00ffc62d8022
root <lightfaith@users.noreply.github.com>
2017-04-14 11:54:39 +0200

    install script fix, rhel support

--- install_debian.sh
+++ install_debian.sh
@@ -2,14 +2,14 @@
 
 # install mandatory packages
 sudo apt-get update &&
-sudo apt-get install python3 python3-pip python3-reportlab python3-paramiko
+sudo apt-get install wget unzip python3 python3-pip python3-reportlab python3-paramiko
 
 LOCATION=`pwd`
 # get binwalk
 cd /tmp/
 wget https://github.com/devttys0/binwalk/archive/master.zip
 unzip master.zip
-cd binwalk-master && sudo python3 setup.py uninstall && sudo python3 setup.py install && sudo ./deps.sh
+cd binwalk-master && sudo python3 setup.py uninstall && sudo python3 setup.py install && sudo ./deps.sh || echo '[-] Binwalk installation failed, extracting functionality might not be successful. Consider installing desired packages manually.'
 cd "$LOCATION"
 # set empty database files
 for db in 'analysis' 'checksum' 'dict' 'module' 'vuln'; do
---
+++ install_rhel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# install mandatory packages
+yum install epel-release
+yum install wget unzip python34 python34-pip python-reportlab python34-paramiko
+
+LOCATION=`pwd`
+# get binwalk
+cd /tmp/
+wget https://github.com/devttys0/binwalk/archive/master.zip
+unzip master.zip
+cd binwalk-master && sudo python3 setup.py uninstall && sudo python3 setup.py install && sudo ./deps.sh
+cd "$LOCATION"
+# set empty database files
+for db in 'analysis' 'checksum' 'dict' 'module' 'vuln'; do
+	cp -i $db.db.empty $db.db;
+done
+
+# install necessary python packages
+sudo pip3 install paramiko reportlab
